dedad97aa61f490216861f5b6123e867c55868bd
Yulia-Yasinska <yasinskayulia7@gmail.com>
2025-07-21 20:21:44 +0300

    third_commit

--- second_exercise.py
+++ second_exercise.py
@@ -1,11 +1,11 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or quantity <= 0:
-        print("Введіть min, max та quantity у заданих обмеженнях!")
+    if min < 1 or max > 1000 or quantity <= 0 or max < min or quantity > max - min:
+        print("Введіть коректні min, max та quantity!")
         return None
     lottery_list = random.sample(range(min, max + 1), quantity)
     return sorted(lottery_list)
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
